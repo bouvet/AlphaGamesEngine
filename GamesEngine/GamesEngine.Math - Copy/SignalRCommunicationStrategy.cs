3b77d4477e111fa0059d993d32826c83ae162785
roger.berheim <roger.berheim@bouvet.no>
2023-05-25 14:32:42 +0200

    Added implementation for Communication.

---
+++ GamesEngine/GamesEngine.Math - Copy/SignalRCommunicationStrategy.cs
@@ -0,0 +1,16 @@
+﻿public class SignalRCommunicationStartegy : ICommunication
+{
+    public ICommunicationStrategy CommunicationStrategy => throw new NotImplementedException();
+
+    public ICommandDispatcher CommandDispatcher => throw new NotImplementedException();
+
+    public void OnMessage(IQuery query)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SendMessage(ICommand command)
+    {
+        throw new NotImplementedException();
+    }
+}

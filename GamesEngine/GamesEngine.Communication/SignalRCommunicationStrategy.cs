f5fb9d9f6c76efca5d73cce0bac69a50e9002005
haavard-hoijord <havard.hoijord@bouvet.no>
2023-05-26 08:41:08 +0200

    Rename folders for Patterns and Communication

---
+++ GamesEngine/GamesEngine.Communication/SignalRCommunicationStrategy.cs
@@ -0,0 +1,23 @@
+﻿using GamesEngine.Patterns.Command;
+
+namespace GamesEngine.Service.Communication
+{
+    public class SignalRCommunicationStartegy : ICommunication
+    {
+        public ICommunicationStrategy CommunicationStrategy => throw new NotImplementedException();
+
+        public ICommandDispatcher CommandDispatcher => throw new NotImplementedException();
+
+        ICommandDispatcher ICommunication.CommandDispatcher => throw new NotImplementedException();
+
+        public void OnMessage(IQuery query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SendMessage(ICommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

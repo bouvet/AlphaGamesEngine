d1df9547c63e44f36596bd6c742365ff2a229daa
haavard-hoijord <havard.hoijord@bouvet.no>
2023-05-31 14:03:34 +0200

    SceneGraph BinTree interface

---
+++ GamesEngine/GamesEngine.Service/Game/Graph/GraphBinTree.cs
@@ -0,0 +1,47 @@
+namespace GamesEngine.Service.Game.Graph;
+
+public interface IGraphBinTree<T1, T2>
+{
+    T2 Get(T1 key);
+    void Add(T1 key, T2 value);
+    void Remove(T1 key);
+    void Update(T1 key, T2 value);
+    List<T1> GetKeys();
+    List<T2> GetValues();
+}
+
+public class GraphBinTree<T1, T2> : IGraphBinTree<T1, T2>
+{
+    SortedDictionary<T1, T2> tree = new SortedDictionary<T1, T2>();
+
+    public T2 Get(T1 key)
+    {
+        return tree[key];
+    }
+
+    public void Add(T1 key, T2 value)
+    {
+        tree.Add(key, value);
+    }
+
+    public void Remove(T1 key)
+    {
+        tree.Remove(key);
+    }
+
+    public void Update(T1 key, T2 value)
+    {
+        Remove(key);
+        Add(key, value);
+    }
+
+    public List<T1> GetKeys()
+    {
+        return tree.Keys.ToList();
+    }
+
+    public List<T2> GetValues()
+    {
+        return tree.Values.ToList();
+    }
+}

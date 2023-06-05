6c91a63f7bdd95ccb89c584034701a64ffa9226c
rebeccasjoen <103406013+rebeccasjoen@users.noreply.github.com>
2023-06-05 12:50:09 +0200

    Start test for Move command

---
+++ GamesEngine/GamesEngine.Tests/CommunicationCommandHandlersTests.cs
@@ -0,0 +1,30 @@
+﻿using GamesEngine.Patterns.Query;
+using GamesEngine.Service.Communication.CommandHandlers;
+using GamesEngine.Tests.Fakes.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesEngine.Tests
+{
+
+    [TestFixture]
+    public class CommunicationCommandHandlersTests
+    {
+
+        // Mocks: player, client object to update position
+
+        MovePlayerCommandMock commandMock = new MovePlayerCommandMock();
+        //ICommandCallback<string> callback;
+
+        [Test]
+        public void ShouldBeAbleToUpdatePlayerPosition()
+        {
+            MovePlayerCommandHandler commandHandler = new MovePlayerCommandHandler();
+            //commandHandler.Handle(commandMock, callback);
+        }
+
+    }
+}

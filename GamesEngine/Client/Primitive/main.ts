73637377cc417ee6a2d14228df6f008f118e04f9
rbaerheim <rgbhm@hotmail.com>
2023-06-06 09:53:09 +0200

    Added more

--- GamesEngine/Client/Primitive/main.ts
+++ GamesEngine/Client/Primitive/main.ts
@@ -1,33 +1,76 @@
 import "./node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./node_modules/bootstrap/dist/css/bootstrap.min.css";
+
+import * as signalR from "@microsoft/signalr";
+
+export const connection = new signalR.HubConnectionBuilder().withUrl('https://localhost:7247/gamehub', {
+            skipNegotiation: true,
+            transport: signalR.HttpTransportType.WebSockets
+            }).build();
+
+
+export async function startSignalR() {
+  connection.start()
+            .then(() => {
+              console.log("SignalR connected");
+              let message = {Type: "FetchDynamicObjects"}
+              connection.send("SendMessage", JSON.stringify(message));
+            }).catch((error) => console.log(error));
+}
 
 const canvas = document.getElementById('myCanvas') as HTMLCanvasElement | null;
 if (canvas) {
   const context = canvas.getContext('2d');
   
-    let x = 50; // Starting x position
-    let y = 50; // Starting y position
-    const size = 50; // Size of the square
+    let playerStartX = 100;
+    let playerStartY = 100;
+
+    function onDocumentKeyDown(event: KeyboardEvent) {};
+    function onDocumentKeyUp(event: KeyboardEvent) {};
+
+    function keyDownHandler(event: KeyboardEvent) {
+      if (event.key === 'Right' || event.key === 'ArrowRight') {
+          // Left arrow
+          playerStartX += 1;
+      } else if (event.key === 'Left' || event.key === 'ArrowLeft') {
+          // Right arrow
+          playerStartX -= 1;
+      } else if (event.key === 'Up' || event.key === 'ArrowUp') {
+          // Up arrow
+          playerStartY -= 1;
+      } else if (event.key === 'Down' || event.key === 'ArrowDown') {
+          // Down arrow
+          playerStartY += 1;
+      }
+  }
+    document.addEventListener("keydown", onDocumentKeyDown, false);
+    document.addEventListener("keyup", onDocumentKeyUp, false);
+    document.addEventListener('keydown', (event) => {
+      keyDownHandler(event);
+    }, false);
+
+
+    const size = 10;
     function drawSquare() {
-        // Clear the previous frame
+        
         if (context && canvas) {
+            // Clear the previous frame
             context.clearRect(0, 0, canvas.width, canvas.height);
 
             context.beginPath();
-            context.rect(x, y, size, size);
+            context.rect(playerStartX, playerStartY, size, size);
             context.fillStyle = "#FF0000";
             context.fill();
             context.closePath();
-
-            x += 1;
-            y += 1;
         }
     }
 
     function animate() {
-    drawSquare();
-    requestAnimationFrame(animate); // Call animate again for the next frame
+      drawSquare();
+      requestAnimationFrame(animate);
     }
 
     animate();
+    startSignalR();
   }
 

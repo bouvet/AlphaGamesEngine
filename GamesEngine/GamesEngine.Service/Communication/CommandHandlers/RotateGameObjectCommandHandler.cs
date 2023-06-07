360d40258bc5153c824cfc6fc2a3dfa3ba4ab89f
rebeccasjoen <103406013+rebeccasjoen@users.noreply.github.com>
2023-06-07 10:45:50 +0200

    Rotate object from master

--- GamesEngine/GamesEngine.Service/Communication/CommandHandlers/RotateGameObjectCommandHandler.cs
+++ GamesEngine/GamesEngine.Service/Communication/CommandHandlers/RotateGameObjectCommandHandler.cs
@@ -11,20 +11,7 @@ namespace GamesEngine.Service.Communication.CommandHandlers
     {
         public void Handle(RotateGameObjectCommand command, ICommandCallback<string> callback)
         {
-            IGameObject gameObject = GameHandler.GetClient(command.ConnectionId).PlayerGameObject;
-
-            var targetX = command.MousePositionX - gameObject.WorldMatrix.GetPosition().GetX();
-            var targetY = command.MousePositionY - gameObject.WorldMatrix.GetPosition().GetY();
-            double offsetAngle = System.Math.PI / 2;
-            double angle = System.Math.Atan2(targetY, targetX) + offsetAngle;
-            //Console.WriteLine($"angle {angle}");
-
-            Quaternion rotation = Quaternion.CreateFromYawPitchRoll(0, (float)angle, 0);
-            Vector3 rotationVector = new Vector3(gameObject.WorldMatrix.GetRotation().GetX(), gameObject.WorldMatrix.GetRotation().GetY(), gameObject.WorldMatrix.GetRotation().GetZ());
-            Vector3 rotatedVector3 = Vector3.Transform(rotationVector, rotation);
-            //Console.WriteLine($"Rot vector{rotatedVector3.X} {rotatedVector3.Y} {rotatedVector3.Z}");
-            IVector rotatedVector = new Math.Vector(rotatedVector3.X, rotatedVector3.Y, rotatedVector3.Z);
-            gameObject.WorldMatrix.SetRotation(rotatedVector);
+            IGameObject gameObject = GameHandler.GetGame(command.ConnectionId).FindGameObject(GameHandler.GetClient(command.ConnectionId).PlayerGameObject.Id);
 
             if (gameObject != null)
             {
@@ -32,11 +19,10 @@ namespace GamesEngine.Service.Communication.CommandHandlers
                 IVector rotationVector = new Math.Vector(1, rot, 0);
                 gameObject.WorldMatrix.SetRotation(rotationVector);
                 callback.OnSuccess("success");
+                return;
             }
-            else
-            {
-                callback.OnFailure();
-            }
+
+            callback.OnFailure();
         }
 
         private static float CalculateRotation(IVector vector, float targetX, float targetY)
